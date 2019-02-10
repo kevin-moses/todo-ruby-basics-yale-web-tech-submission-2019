d845dae606b207e7ef9e303db7ef79067c65ea83
Kevin Moses <kevin.moses@yale.edu>
2019-02-10 03:48:36 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,21 +1,25 @@
 def division(num1, num2)
- 
+ num3 = num1/num2
+ num3
 end
 
 def assign_variable(value)
-  
+  var = values
+  var
 end
 
-def argue
-  
+def argue(arg)
+  arg
 end
 
-def greeting
-
+def greeting(arg1, arg2)
+  arg1
+  arg2
 end
 
 def return_a_value
-
+  nice = "Nice"
+  nice
 end
 
 def last_evaluated_value
